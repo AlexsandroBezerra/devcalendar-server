97ec27ba86680c74cec1bd174a84c2c98032bc8b
Alexsandro G Bezerra <alexsandro.g.bezerra@gmail.com>
2020-12-25 11:14:33 -0300

    feat: add babel

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-typescript'
+  ],
+  plugins: [
+    ['module-resolver', {
+      alias: {
+        '@shared': './src/shared',
+        '@config': './src/config',
+        '@modules': './src/modules'
+      }
+    }],
+    'babel-plugin-transform-typescript-metadata',
+    ['@babel/plugin-proposal-decorators', { 'legacy': true }],
+    ['@babel/plugin-proposal-class-properties', { 'loose': true }]
+  ],
+  ignore: [
+    "**./*.spec.ts"
+  ]
+}
