811b46509a44f77df484a435223e6b29ea92aebe
Alexsandro G Bezerra <alexsandro.g.bezerra@gmail.com>
2021-01-04 12:46:54 -0300

    feat: setup secure jwt

--- src/modules/users/providers/AuthenticationProvider/implementations/AuthenticationProvider.ts
+++ src/modules/users/providers/AuthenticationProvider/implementations/AuthenticationProvider.ts
@@ -1,15 +1,17 @@
 import jwt from 'jsonwebtoken'
 
+import authConfig from '@config/auth'
+
 import IAuthenticationProvider from '../models/IAuthenticationProvider'
 
 class AuthenticationProvider implements IAuthenticationProvider {
   public sign(payload: string | object): string {
-    return jwt.sign(payload, 'secret_key')
+    return jwt.sign(payload, authConfig.secret, authConfig.config)
   }
 
   public async verify(payload: string): Promise<string | object | void> {
     return new Promise((resolve, reject) => {
-      jwt.verify(payload, 'secret_key', (error, decoded) =>
+      jwt.verify(payload, authConfig.secret, (error, decoded) =>
         error ? reject(error) : resolve(decoded)
       )
     })
