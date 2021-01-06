d652391fea7ca7a8a70d8a0bf7610fad6d6646d9
Alexsandro G Bezerra <alexsandro.g.bezerra@gmail.com>
2021-01-06 09:33:36 -0300

    feat: add cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata'
+import cors from 'cors'
 import express from 'express'
 import 'express-async-errors'
 
@@ -12,6 +13,7 @@ createConnection()
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 app.use(errorHandler)
