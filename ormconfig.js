20e0200c16254fee2c7f9a484169007bdddae52d
Alexsandro G Bezerra <alexsandro.g.bezerra@gmail.com>
2021-01-06 09:32:06 -0300

    chore: add production database connection

--- ormconfig.js
+++ ormconfig.js
@@ -1,11 +1,11 @@
 require('dotenv/config')
 
-module.exports = {
+const devConfig = {
   type: 'postgres',
   host: process.env.POSTGRES_HOST,
   port: process.env.POSTGRES_PORT,
   username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASS,
+  password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB_NAME,
   entities: ['./src/modules/**/infra/typeorm/entities/*.ts'],
   migrations: ['./src/shared/infra/typeorm/migrations/*.ts'],
@@ -13,3 +13,19 @@ module.exports = {
     migrationsDir: './src/shared/infra/typeorm/migrations'
   }
 }
+
+const prodConfig = {
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB_NAME,
+  entities: ['./dist/modules/**/infra/typeorm/entities/*.js'],
+  migrations: ['./dist/shared/infra/typeorm/migrations/*.js'],
+  cli: {
+    migrationsDir: './src/shared/infra/typeorm/migrations'
+  }
+}
+
+module.exports = process.env.NODE_ENV === 'production' ? prodConfig : devConfig
