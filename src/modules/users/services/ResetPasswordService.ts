929cb2bc59a7f92d53130d675b3952b17bd37610
Alexsandro G Bezerra <alexsandro.g.bezerra@gmail.com>
2021-01-09 16:35:09 -0300

    feat: add ResetPasswordService

---
+++ src/modules/users/services/ResetPasswordService.ts
@@ -0,0 +1,52 @@
+import { isAfter, addHours } from 'date-fns'
+import { inject, injectable } from 'tsyringe'
+
+import AppError from '@shared/errors/AppError'
+
+import IHashProvider from '../providers/HashProvider/models/IHashProvider'
+import IUsersRepository from '../repositories/IUsersRepository'
+import IUserTokensRepository from '../repositories/IUserTokensRepository'
+
+interface IRequest {
+  password: string
+  token: string
+}
+
+@injectable()
+export default class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider
+  ) {}
+
+  public async execute({ token, password }: IRequest): Promise<void> {
+    const userToken = await this.userTokensRepository.findByToken(token)
+
+    if (!userToken) {
+      throw new AppError('UNAUTHORIZED', 'User token does not exists', 401)
+    }
+
+    const user = await this.usersRepository.findById(userToken.userId)
+
+    if (!user) {
+      throw new AppError('CONFLICT', 'User does not exists', 409)
+    }
+
+    const tokenCreatedAt = userToken.createdAt
+    const compareDate = addHours(tokenCreatedAt, 2)
+
+    if (isAfter(Date.now(), compareDate)) {
+      throw new AppError('UNAUTHORIZED', 'Token expired', 401)
+    }
+
+    user.password = await this.hashProvider.generateHash(password)
+
+    await this.usersRepository.update(user)
+  }
+}
--- src/shared/errors/AppError.ts
+++ src/shared/errors/AppError.ts
@@ -5,7 +5,7 @@ class AppError {
 
   public readonly statusCode: number
 
-  constructor(code: string, message: string, statusCode: number) {
+  constructor(code: string, message: string, statusCode = 400) {
     this.code = code
     this.message = message
     this.statusCode = statusCode
