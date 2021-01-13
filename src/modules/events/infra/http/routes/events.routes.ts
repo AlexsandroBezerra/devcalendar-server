9b30f44d584225c54563a6b0beb8313c08646a61
Alexsandro G Bezerra <alexsandro.g.bezerra@gmail.com>
2021-01-13 19:38:33 -0300

    feat: add event route validation

--- src/modules/events/infra/http/routes/events.routes.ts
+++ src/modules/events/infra/http/routes/events.routes.ts
@@ -1,6 +1,7 @@
 import { parseISO } from 'date-fns'
 import { Router } from 'express'
 import { container } from 'tsyringe'
+import * as Yup from 'yup'
 
 import CreateEventService from '@modules/events/services/CreateEventService'
 
@@ -14,14 +15,29 @@ eventRouter.post('/', async (request, response) => {
 
   const formattedDate = parseISO(date)
 
-  const event = await createEvent.execute({
+  const schema = Yup.object().shape({
+    userId: Yup.string().required(),
+    title: Yup.string().required('Title is required'),
+    date: Yup.date().required(),
+    description: Yup.string(),
+    from: Yup.string(),
+    to: Yup.string()
+  })
+
+  const data = {
     userId,
     title,
     date: formattedDate,
     description,
     from,
     to
+  }
+
+  await schema.validate(data, {
+    abortEarly: false
   })
+
+  const event = await createEvent.execute(data)
 
   return response.json(event)
 })
